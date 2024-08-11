cc360660d98dc2fc7d33e6d0ebbd144fe31fb492
oamilynn <emerald.green.cat@gmail.com>
2024-08-11 17:28:02 +0200

    206 reversed linked list

---
+++ src/com/leetcode/grind75/week2/ReverseLinkedList206.java
@@ -0,0 +1,21 @@
+package com.leetcode.grind75.week2;
+
+import com.leetcode.model.ListNode;
+
+public class ReverseLinkedList206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode last = head.next;
+        ListNode newHead = head;
+        while (last != null) {
+            ListNode tmp = newHead;
+            newHead = last;
+            last = newHead.next;
+            newHead.next = tmp;
+        }
+        head.next = null;
+        return newHead;
+    }
+}
