380b150c20b29b6a817807279c763a52ed2d8cb7
oamilynn <emerald.green.cat@gmail.com>
2024-08-11 17:27:08 +0200

    169 majority element

---
+++ src/com/leetcode/grind75/week2/MajorityElement169.java
@@ -0,0 +1,24 @@
+package com.leetcode.grind75.week2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement169 {
+
+    // TODO: inefficient
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> numbers = new HashMap<>();
+        int majority = (nums.length + 1) / 2;
+        for (int n : nums) {
+            Integer cnt = numbers.get(n);
+            if (cnt == null) {
+                cnt = 0;
+            }
+            numbers.put(n, ++cnt);
+            if (majority <= cnt) {
+                return n;
+            }
+        }
+        return -1;
+    }
+}
